438d9e13c6c759f1fabc4731f396975cf787b65f
programming hero <naofelbadsha@gmail.com>
2023-07-14 10:58:58 +0100

    added header section

---
+++ main.js
@@ -0,0 +1,25 @@
+/*----------Header------Section--------Start--------*/
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active'); 
+
+
+
+    /*-------Scroll---------Button--------Start------*/
+    if(window.scrollY > 60){
+      document.querySelector('#scroll-top').classList.add('active');
+    }
+    else{
+      document.querySelector('#scroll-top').classList.remove('active');
+    }
+    /*-------Scroll---------Button--------End--------*/
+};
+/*----------Header------Section--------End--------*/
